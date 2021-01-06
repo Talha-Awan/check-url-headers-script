fd4d45843480fe1191b3053c9eb7a56314602f3b
Muhammad Talha Awan <talhaawan70@gmail.com>
2021-01-06 18:49:27 +0500

    Update search_header.sh

--- search_header.sh
+++ search_header.sh
@@ -1,11 +1,11 @@
-for URL in `cat /home/faisal/Desktop/site.txt`;
+for URL in `cat /home/Talha/Desktop/site.txt`;
 do
 echo $URL
 if ((  $(curl -IL $URL --stderr - | grep -c "x-powered-by: BionicWP") > 0 )) ;
 then
-echo $URL >> /home/faisal/Desktop/inbionic.txt
+echo $URL >> /home/Talha/Desktop/inbionic.txt
 else
-echo $URL >> /home/faisal/Desktop/notinbio.txt
+echo $URL >> /home/Talha/Desktop/notinbio.txt
 fi
 echo " "
 done
